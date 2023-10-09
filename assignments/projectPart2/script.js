763b87997572dcfa4c2b42ccf363c4aded57709b
raphaelattfield <raphaelattfield@hotmail.co.uk>
2023-10-09 15:29:36 -0400

    finished header and footer

---
+++ assignments/projectPart2/script.js
@@ -0,0 +1,13 @@
+const toggleNav = () => {
+  document.getElementById("nav-items").classList.toggle("hide-small");
+};
+
+
+
+
+
+window.onload = () => {
+  document.getElementById("hamburger").onclick = toggleNav;
+}
+
+
