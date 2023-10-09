0848628f4c4d915001247377b99d372dc2d79881
raphaelattfield <raphaelattfield@hotmail.co.uk>
2023-10-09 15:51:08 -0400

    added clickable logos with links

--- assignments/projectPart2/script.js
+++ assignments/projectPart2/script.js
@@ -2,12 +2,29 @@ const toggleNav = () => {
   document.getElementById("nav-items").classList.toggle("hide-small");
 };
 
+const goToBookingsPage = () => {
+  window.location.href = "bookings.html";
+};
+
+const goToFacebookPage = () => {
+  window.location.href = "http://facebook.com";
+};
+
+const goToTwitterPage = () => {
+  window.location.href = "http://twitter.com";
+};
+
+const goToInstaPage = () => {
+  window.location.href = "http://instagram.com";
+};
 
 
 
 
 window.onload = () => {
   document.getElementById("hamburger").onclick = toggleNav;
+  document.getElementById("button-book").onclick = goToBookingsPage;
+  document.getElementById("facebook-logo").onclick = goToFacebookPage;
+  document.getElementById("twitter-logo").onclick = goToTwitterPage;
+  document.getElementById("insta-logo").onclick = goToInstaPage;
 }
-
-
