b955c0a224155917c45fb4af55ac45e037f3e772
raphaelattfield <raphaelattfield@hotmail.co.uk>
2023-10-22 16:46:57 -0400

    created menus. finished assignment

--- project/htmlandcss/script.js
+++ project/htmlandcss/script.js
@@ -6,17 +6,6 @@ const goToBookingsPage = () => {
   window.location.href = "bookings.html";
 };
 
-const goToFacebookPage = () => {
-  window.location.href = "http://facebook.com";
-};
-
-const goToTwitterPage = () => {
-  window.location.href = "http://twitter.com";
-};
-
-const goToInstaPage = () => {
-  window.location.href = "http://instagram.com";
-};
 
 
 
@@ -24,7 +13,5 @@ const goToInstaPage = () => {
 window.onload = () => {
   document.getElementById("hamburger").onclick = toggleNav;
   document.getElementById("button-book").onclick = goToBookingsPage;
-  document.getElementById("facebook-logo").onclick = goToFacebookPage;
-  document.getElementById("twitter-logo").onclick = goToTwitterPage;
-  document.getElementById("insta-logo").onclick = goToInstaPage;
+
 }
